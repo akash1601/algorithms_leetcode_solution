26d8e50955d58578531c55b71cd714211db30029
akash1601 <akashshah1601@gmail.com>
2020-06-19 01:26:56 -0700

    Update insertion.py

--- divide and concur/insertion.py
+++ divide and concur/insertion.py
@@ -1,6 +1,7 @@
 # Function to do insertion sort
 def insertionSort(arr):
     # Traverse through 1 to len(arr)
+    # this is insertion sort
     for i in range(1, len(arr)):
         key = arr[i]
         # Move elements of arr[0..i-1], that are
