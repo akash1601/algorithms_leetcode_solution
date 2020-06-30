44225a0341b62d788604e0b70b16b89e377fbdc0
akash1601 <akashshah1601@gmail.com>
2020-06-30 10:48:24 -0700

    leetcode 3 sum closet solution

---
+++ solutions/3sumCloset.py
@@ -0,0 +1,24 @@
+def threeSumCloset(nums,target):
+	nums.sort()
+	result = nums[0] + nums[1] + nums[2]
+	for i in range(len(nums)-2):
+		j, k = i+1, len(nums) -1
+		sum = nums[i] + nums[j] + nums[k]
+		while j < k:
+			if sum == target:
+				return sum
+
+			if abs(sum - target) < abs(result - target):
+				result = sum
+
+			if sum < target:
+				j += 1
+
+			elif sum > target:
+				k -= 1
+
+	print(result)
+
+threeSumCloset([-1,2,0,5,6,1,-4],8)
+
+
