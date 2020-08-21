cea823e50a58f0fe9e68e6ab863d0762b3b092d5
akash1601 <akashshah1601@gmail.com>
2020-08-21 13:11:00 -0700

    Create breakPalindrome.py

---
+++ solutions/breakPalindrome.py
@@ -0,0 +1,8 @@
+def breakPalindrome(palindrome):
+        if len(palindrome) == 1:
+            return ""
+        for i in range(len(palindrome) // 2):
+            if palindrome[i] != 'a':
+                return palindrome[:i] + 'a' + palindrome[i + 1:]
+        return ("IMPOSSIBLE")
+breakPalindrome("abaacaaba")
