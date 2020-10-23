e2cb5eb2d1227a88353afa3c319942223384b442
akash1601 <akashshah1601@gmail.com>
2020-10-22 23:01:34 -0700

    Added postorder and preorder

--- solutions/inorderTravelTree.py
+++ solutions/inorderTravelTree.py
@@ -8,3 +8,27 @@ def helper(node, res):
 		self.helper(root.left, res)
 		res.append(node.val)
 		self.helper(root.right, res)
+
+#postorder traverssal left -> right -> Node
+def postorderTravel(self, root):
+	res = []
+	self.helper(root, res)
+	return res
+
+def helper(node, res):
+	if node:
+		self.helper(root.left, res)
+		self.helper(root.right, res)
+		res.append(node.val)
+
+#preorder  traversal Node -> left -> right
+def postorderTravel(self, root):
+	res = []
+	self.helper(root, res)
+	return res
+
+def helper(node, res):
+	if node:
+		res.append(node.val)
+		self.helper(root.left, res)
+		self.helper(root.right, res)
