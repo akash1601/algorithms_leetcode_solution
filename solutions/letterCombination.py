d8eae252894d40df4e0e74a96e7008c3b1c907f0
akash1601 <akashshah1601@gmail.com>
2020-11-16 22:39:32 -0800

    Create letterCombination.py

---
+++ solutions/letterCombination.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        
+        nums_to_letters = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz"
+        }
+        
+        combs = [""]
+        for digit in digits:
+            new_combs = []
+           
+            for comb in combs:
+                for letter in nums_to_letters[int(digit)]:
+                    new_combs.append(comb + letter)
+                    
+            combs = new_combs
+            
+        return combs
