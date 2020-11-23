952ce4e843c62f57752418b7f2526df1eb946d53
akash1601 <akashshah1601@gmail.com>
2020-11-22 22:16:46 -0800

    Create findOcurrences.py

---
+++ solutions/findOcurrences.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        arr = text.split()
+        ans = []
+        for i in range(2, len(arr)):
+            if arr[i - 2] == first and arr[i -1] == second:
+                ans += arr[i],
+        return ans
