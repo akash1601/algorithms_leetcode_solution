4e57a24c0f73bdd9a6bebd7f02c51c73b8fb9c60
akash1601 <akashshah1601@gmail.com>
2021-02-23 22:33:26 -0800

    Update medianSortedArray.py

--- solutions/medianSortedArray.py
+++ solutions/medianSortedArray.py
@@ -1,14 +1,14 @@
-def median(num1,num2):
-	nums3 = num1 + num2
-	nums3.sort()
-	l = len(nums3)
-	med = l//2
-	if (l % 2 == 0):
-		 print((nums3[med-1] + nums3[med])/2)
-	else:
-		print(nums3[med])
-        
-
-num1 = [1,2,3]
-num2 = [2]
-median(num1,num2)
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+   
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            print(nums1[:n])
+            nums1[:n] = nums2[:n]
+            
