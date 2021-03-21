aaf0c6406804571ac2ffe1c708d42312e50d4728
akash1601 <akashshah1601@gmail.com>
2021-03-21 16:16:02 -0700

    Create pathSum2.py

---
+++ recursive/pathSum2.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        res = []
+        self.dfs(root, targetSum, [], res)
+        return res
+    
+    def dfs(self, root, sum, ls, res):
+        if root:
+            if not root.left and not root.right and  sum == root.val:
+                ls.append(root.val)
+                res.append(ls)
+            self.dfs(root.left, sum - root.val, ls + [root.val], res)
+            self.dfs(root.right, sum - root.val, ls + [root.val], res)
+
+
